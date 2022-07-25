b354db32e7d3b408402c413cca1d0780f9813cd2
Lazy Mind <srjkadel@gmail.com>
2022-07-25 15:59:01 +0545

    added bird head and gesturedetect with math

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flappy_bird/widget/bird_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,16 +11,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static double birdYaxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = birdYaxis;
+  bool hasGameStart = false;
+
+  void startGame() {
+    hasGameStart = true;
+    Timer.periodic(Duration(milliseconds: 50), (timer) {
+      time += 0.04;
+      height = -4.9 * time * time + 3 * time;
+      setState(() {
+        birdYaxis = initialHeight - height;
+      });
+      if (birdYaxis > 1) {
+        timer.cancel();
+        hasGameStart = false;
+      }
+    });
+  }
+
+  void jump() {
+    time = 0;
+    initialHeight =  birdYaxis;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Expanded(
-            flex: 2,
-              child: Container(
-            color: Colors.blue,
-          )),
+              flex: 2,
+              child: GestureDetector(
+                onTap: () {
+                  if (hasGameStart) {
+                    jump();
+                  } else {
+                    startGame();
+                  }
+                },
+                child: AnimatedContainer(
+                  alignment: Alignment(0, birdYaxis),
+                  duration: Duration(milliseconds: 0),
+                  color: Colors.blue,
+                  child: MyBird(),
+                ),
+              )),
           Expanded(
               child: Container(
             color: Colors.green,
---
+++ lib/widget/bird_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class MyBird extends StatelessWidget {
+  const MyBird({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("img/bird.png", height: 80, width: 80,);
+  }
+}
